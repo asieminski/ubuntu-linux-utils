cbc20ae53786ec2ecff725dbbc2afb42a4548918
Antoni Sieminski <a.m.sieminski@gmail.com>
2022-10-25 00:33:06 +0100

    add alacritty

--- install-utils.sh
+++ install-utils.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt install awesome compton nitrogen dmenu ranger htop -y
 
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
